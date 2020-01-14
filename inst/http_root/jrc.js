533fdef7a06b83abf3165f29b2f86ec0a34800cf
Svetlana Ovchinnikova <s.ovchinnikova@zmbh.uni-heidelberg.de>
2020-01-14 18:10:04 +0000

    version 0.3.0

--- inst/http_root/jrc.js
+++ inst/http_root/jrc.js
@@ -91,6 +91,10 @@ jrc.ws.addEventListener( "message", function(event) {
 
 		return;
 	}
+	if(msg[0] == "ID") {
+		jrc.id = msg[1]
+		return;
+	}
 	console.log("Unknown message type: " + msg[0])
 	jrc.ws.send("warning('Unknown message type: " + msg[0] + "')");
 } );
@@ -102,17 +106,17 @@ jrc.sendCommand = function(command) {
 	jrc.ws.send(JSON.stringify(["COM", command]));
 }
 
-jrc.sendData = function(variableName, variable) {
-	jrc.ws.send(JSON.stringify(["DATA", variableName, JSON.stringify(variable)]));
+jrc.sendData = function(variableName, variable, internal) {
+	jrc.ws.send(JSON.stringify(["DATA", variableName, JSON.stringify(variable), internal]));
 }
 
 //args must be object (to be converted to names list in R)
-jrc.callFunction = function(functionName, args, assingTo, package) {
-	jrc.ws.send(JSON.stringify(["FUN", functionName, JSON.stringify(args), assingTo, package]));
+jrc.callFunction = function(functionName, args, assingTo, package, internal) {
+	jrc.ws.send(JSON.stringify(["FUN", functionName, JSON.stringify(args), assingTo, package, internal]));
 }
 
 jrc.notifyStorage = function(id) {
-	alert("Your command has been stored. To execute it, please, type 'authorize(id = \"" + id + "\")'. " + 
+	alert("Your command has been stored. To execute it, please, run the 'authorize' function as it is shown in your R sesion. " + 
 			"Use functions 'allowVariables' and 'allowFunctions' to permit automatic execution of " +
 			"your commands.")
 }
